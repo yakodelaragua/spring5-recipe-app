7c348a9549e2a522f3b1145d5af1a6d36d5aac18
Yakodelaragua <araycdd@gmail.com>
2023-01-23 16:14:28 +0100

    Recipe display completed

--- src/main/java/yara/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/yara/springframework/spring5recipeapp/controllers/IndexController.java
@@ -1,31 +1,27 @@
 package yara.springframework.spring5recipeapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import yara.springframework.spring5recipeapp.domain.Category;
 import yara.springframework.spring5recipeapp.domain.UnitOfMeasure;
 import yara.springframework.spring5recipeapp.repositories.CategoryRepository;
 import yara.springframework.spring5recipeapp.repositories.UnitOfMeasureRepository;
+import yara.springframework.spring5recipeapp.services.RecipeService;
 
 import java.util.Optional;
 
 @Controller
 public class IndexController {
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+    private final RecipeService recipeService;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(){
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasureRepositoryOptional = unitOfMeasureRepository.findByDescription("Cup");
-        System.out.println("Cat id is: " +  categoryOptional.get().getId());
-        System.out.println("UOM id is: " +  unitOfMeasureRepositoryOptional.get().getId());
-
+    public String getIndexPage(Model model){
+        model.addAttribute("recipes", recipeService.getRecipes());
 
         return "index";
     }
---
+++ src/main/java/yara/springframework/spring5recipeapp/services/RecipeService.java
@@ -0,0 +1,9 @@
+package yara.springframework.spring5recipeapp.services;
+
+import yara.springframework.spring5recipeapp.domain.Recipe;
+
+import java.util.Set;
+
+public interface RecipeService {
+    Set<Recipe> getRecipes();
+}
