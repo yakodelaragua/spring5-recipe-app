9839c76d274dc0701ded5be9480d34bc2601dead
Yakodelaragua <araycdd@gmail.com>
2023-01-23 15:02:07 +0100

    insert data.sql

--- src/main/java/yara/springframework/spring5recipeapp/Spring5RecipeAppApplication.java
+++ src/main/java/yara/springframework/spring5recipeapp/Spring5RecipeAppApplication.java
@@ -2,6 +2,7 @@ package yara.springframework.spring5recipeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class Spring5RecipeAppApplication {
--- src/main/java/yara/springframework/spring5recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/yara/springframework/spring5recipeapp/repositories/UnitOfMeasureRepository.java
@@ -1,6 +1,7 @@
 package yara.springframework.spring5recipeapp.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import yara.springframework.spring5recipeapp.domain.UnitOfMeasure;
 
-public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasureRepository, Long> {
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
 }
