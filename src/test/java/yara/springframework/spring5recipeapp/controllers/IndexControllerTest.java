f4e24756500c592236f045cbcd1307248189f6aa
Yakodelaragua <araycdd@gmail.com>
2023-01-24 10:24:10 +0100

    index controller tests

---
+++ src/test/java/yara/springframework/spring5recipeapp/controllers/IndexControllerTest.java
@@ -0,0 +1,37 @@
+package yara.springframework.spring5recipeapp.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+import yara.springframework.spring5recipeapp.services.RecipeServiceImpl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+class IndexControllerTest {
+    IndexController controller;
+    @Mock
+    RecipeServiceImpl recipeService;
+    @Mock
+    Model model;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        controller = new IndexController(recipeService);
+    }
+
+    @Test
+    void getIndexPage() {
+        String viewName = controller.getIndexPage(model);
+
+        assertEquals("index", viewName);
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+    }
+}
