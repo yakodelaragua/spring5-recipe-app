66937e2355f36bd82c5d7011aa2358abd929e0fb
Yakodelaragua <araycdd@gmail.com>
2023-01-24 15:00:30 +0100

    command object

---
+++ src/main/java/yara/springframework/spring5recipeapp/converters/NotesCommandToNotes.java
@@ -0,0 +1,30 @@
+package yara.springframework.spring5recipeapp.converters;
+
+
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import yara.springframework.spring5recipeapp.commands.NotesCommand;
+import yara.springframework.spring5recipeapp.domain.Notes;
+
+/**
+ * Created by jt on 6/21/17.
+ */
+@Component
+public class NotesCommandToNotes implements Converter<NotesCommand, Notes> {
+
+    @Synchronized
+    @Nullable
+    @Override
+    public Notes convert(NotesCommand source) {
+        if(source == null) {
+            return null;
+        }
+
+        final Notes notes = new Notes();
+        notes.setId(source.getId());
+        notes.setRecipeNotes(source.getRecipeNotes());
+        return notes;
+    }
+}
