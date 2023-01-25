f3cd54013073bb1cef44d39dfc66719c37057a2d
Yakodelaragua <araycdd@gmail.com>
2023-01-25 11:02:19 +0100

    show ingredients

---
+++ src/main/java/yara/springframework/spring5recipeapp/controllers/IngredientController.java
@@ -0,0 +1,25 @@
+package yara.springframework.spring5recipeapp.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import yara.springframework.spring5recipeapp.services.RecipeService;
+
+@Slf4j
+@Controller
+public class IngredientController {
+    private final RecipeService recipeService;
+
+    public IngredientController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+    @GetMapping
+    @RequestMapping("/recipe/{recipeId}/ingredients/")
+    public String listIngredients(@PathVariable String recipeId, Model model) {
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
+        return "recipe/ingredient/list";
+    }
+}
