3e14d3c296b47d47d2c41a6c04a72d07d677b582
Yakodelaragua <araycdd@gmail.com>
2023-01-27 15:44:39 +0100

    Form validation

--- src/main/java/yara/springframework/spring5recipeapp/commands/RecipeCommand.java
+++ src/main/java/yara/springframework/spring5recipeapp/commands/RecipeCommand.java
@@ -1,24 +1,41 @@
 package yara.springframework.spring5recipeapp.commands;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
 import yara.springframework.spring5recipeapp.domain.Difficulty;
 
 import java.util.HashSet;
 import java.util.Set;
+
+
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class RecipeCommand {
     private Long id;
+    @NotBlank
+    @Size(min=3, max=255)
     private String description;
+    @Min(1)
+    @Max(999)
     private Integer prepTime;
+    @Min(1)
+    @Max(999)
     private Integer cookTime;
+    @Min(1)
+    @Max(100)
     private Integer servings;
     private String source;
+    @URL
     private String url;
+    @NotBlank
     private String directions;
     private Set<IngredientCommand> ingredients = new HashSet<>();
     private Byte[] image;
--- src/main/java/yara/springframework/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/yara/springframework/spring5recipeapp/controllers/RecipeController.java
@@ -1,9 +1,11 @@
 package yara.springframework.spring5recipeapp.controllers;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,6 +15,8 @@ import yara.springframework.spring5recipeapp.services.RecipeService;
 @Slf4j
 @Controller
 public class RecipeController {
+
+    private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeform";
 
     private final RecipeService recipeService;
 
@@ -39,7 +43,15 @@ public class RecipeController {
     }
 
     @PostMapping("/recipe/")
-    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            bindingResult.getAllErrors().forEach(objectError -> {
+                log.debug(objectError.toString());
+            });
+
+            return RECIPE_RECIPEFORM_URL;
+        }
+
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
         return "redirect:/recipe/" + savedCommand.getId() + "/show/";
@@ -65,17 +77,5 @@ public class RecipeController {
         return modelAndView;
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(NumberFormatException.class)
-    public ModelAndView handleNumberFormat(Exception exception){
-        log.error("Handling Number Format Exception");
-        log.error(exception.getMessage());
-        ModelAndView modelAndView = new ModelAndView();
 
-        modelAndView.setViewName("400error");
-        modelAndView.addObject("exception", exception);
-
-
-        return modelAndView;
-    }
 }
